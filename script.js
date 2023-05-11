bed91fbe13943cd1ccf52f22dc4f694b9b276631
edyvaca79 <edisonvaca@live.com>
2023-05-11 12:31:47 -0500

    correciones

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+//(function(){
+//    var ct = document.createElement('link');
+//    ct.rel = 'stylesheet';
+//    ct.href = 'https://csswizardry.com/ct/ct.css';
+//    ct.classList.add('ct');
+//    document.head.appendChild(ct);
+//    }());
+
+
 const textArea = document.querySelector(".encriptacion")
 const mensaje = document.querySelector(".mensaje");
 
